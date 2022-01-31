49e4190b6614415709ce9f4a136e6e5462d3a81c
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-01-31 11:11:56 +0100

    Init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Flowers API')
+    .setTitle('Selling pretty flowers')
+    .setVersion('1.0')
+    .addTag('flowers')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
